a09ced9ac1e6f9642acbe1ea4726bafdfb8ff221
mthuy04 <23070553@vnu.edu.vn>
2026-05-05 01:34:08 +0700

    mta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,8 @@ export const metadata: Metadata = {
 
 const gaId = process.env.NEXT_PUBLIC_GA_ID ?? "G-H4FJW15XEV";
 const clarityId = process.env.NEXT_PUBLIC_CLARITY_ID ?? "wlvx0isghh";
+const metaPixelId =
+  process.env.NEXT_PUBLIC_META_PIXEL_ID ?? "1497000078732211";
 
 export default function RootLayout({
   children,
@@ -52,6 +54,31 @@ export default function RootLayout({
             })(window, document, "clarity", "script", "${clarityId}");
           `}
         </Script>
+
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${metaPixelId}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${metaPixelId}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   );
